45e3c55662e782776ff294b02f7b88a9019c160e
Kristjan Rang <mail@kristjanrang.eu>
2011-09-30 18:42:15 +0300

    Test

--- receive.rb
+++ receive.rb
@@ -11,8 +11,6 @@ class Receive < Goliath::API
   def on_body(env, data)
     env.logger.info 'received data: ' + data.size.to_s
     #(env['async-body'] ||= '') << data
-
-    env.stream_send("Heartbeat.\n")
   end  
 
   def response(env)
@@ -20,10 +18,11 @@ class Receive < Goliath::API
     # This timer keeps the connection alive later in the stream when
     # the number generation slows down sufficiently for > 30s response time.
     # Yes, the timer is something like 55s but I like 30s, okay? ;)
-    #keepalive = EM.add_periodic_timer(29) do
-    #  env.stream_send("Heartbeat.\n")
-    #  env.logger.info "heartbeat sent"
-    #end
+    keepalive = EM.add_periodic_timer(29) do
+      env.stream_send("Heartbeat.\n")
+      env.logger.info "heartbeat sent"
+      keepalive.cancel
+    end
     
     # The below cuts off the connection at some point if this is desired.
     # EM.add_timer(30) do
