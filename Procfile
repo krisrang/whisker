8236ff09a8550efd9b7dd8c68f012d1604961b61
Kristjan Rang <mail@kristjanrang.eu>
2011-09-30 15:40:40 +0300

    Test

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bundle exec ruby receive.rb -sv -e production -p $PORT
---
+++ receive.rb
@@ -0,0 +1,43 @@
+require 'rubygems'
+require 'bundler'
+Bundler.require
+
+class Receive < Goliath::API
+  def on_headers(env, headers)
+    env.logger.info 'received headers: ' + headers.inspect
+    env['async-headers'] = headers
+  end
+
+  def on_body(env, data)
+    env.logger.info 'received data: ' + data
+    (env['async-body'] ||= '') << data
+  end
+
+  def on_close(env)
+    env.logger.info 'closing connection'
+  end
+
+  def response(env)
+    # This timer keeps the connection alive later in the stream when
+    # the number generation slows down sufficiently for > 30s response time.
+    # Yes, the timer is something like 55s but I like 30s, okay? ;)
+    keepalive = EM.add_periodic_timer(29) do
+      env.stream_send("Heartbeat.\n")
+    end
+    
+    # The below cuts off the connection at some point if this is desired.
+    # EM.add_timer(30) do
+    #   keepalive.cancel
+    #   
+    #   env.stream_send("End of stream.")
+    #   env.stream_close
+    # end
+
+    #[200, {}, {body: env['async-body'], head: env['async-headers']}]
+    [200, {}, Goliath::Response::STREAMING]
+  end
+
+  def on_close(env)
+    env.logger.info "Connection closed."
+  end
+end
