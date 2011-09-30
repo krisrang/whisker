95dcdca924499967ab4ada06e319edf8ed5d976c
Kristjan Rang <mail@kristjanrang.eu>
2011-09-30 18:37:58 +0300

    Test

--- receive.rb
+++ receive.rb
@@ -14,6 +14,7 @@ class Receive < Goliath::API
   end  
 
   def response(env)
+    env.logger.info 'response'
     # This timer keeps the connection alive later in the stream when
     # the number generation slows down sufficiently for > 30s response time.
     # Yes, the timer is something like 55s but I like 30s, okay? ;)
