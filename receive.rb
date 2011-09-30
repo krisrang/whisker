769b30f2d95acc16ce418420247c9f54cd0409ac
Kristjan Rang <mail@kristjanrang.eu>
2011-09-30 21:02:35 +0300

    Initial framework

--- receive.rb
+++ receive.rb
@@ -2,42 +2,57 @@ require 'rubygems'
 require 'bundler'
 Bundler.require
 
-require 'upload_info'
+$:<< './'
+require 'lib/glue'
+require 'lib/upload_info'
 
 class Receive < Goliath::API
+  use Goliath::Rack::Validation::RequestMethod, %w(POST PUT)
+
   def on_headers(env, headers)
-    env.logger.info 'received headers: ' + headers.inspect
-    #env['async-headers'] = headers
+    id = headers["uploadid"]
 
-    #id = headers[:]
+    unless id.nil?
+      info = UploadInfo.new(id)
+      env['uploadinfo'] = info
 
-    # fetch upload model
-    # initiate multipart
+      # fetch upload model
+      # initiate multipart
+
+      env.logger.info 'initiate upload: ' + id
+    end
   end
 
   def on_body(env, data)
-    env.logger.info 'received data: ' + data.size.to_s
-    #(env['async-body'] ||= '') << data
+    unless env['uploadinfo'].nil?
+      #env.logger.info 'received data: ' + data.size.to_s
 
-    # buffer up 5 mbs worth -> upload part
-    # buffer up 100 kbs worth -> process metadata
+      # buffer up 5 mbs worth -> upload part
+      # buffer up 100 kbs worth -> process metadata
+    end
   end  
 
   def response(env)
     env.logger.info 'response'
 
-    keepalive = EM.add_periodic_timer(1) do
-      env.stream_send("Heartbeat.\n")
-      env.logger.info "heartbeat sent"
+    unless env['uploadinfo'].nil?
+      keepalive = EM.add_periodic_timer(1) do
+        env.stream_send("Heartbeat.\n")
+        env.logger.info "heartbeat sent"
 
-      keepalive.cancel
-      env.stream_send("End of stream.")
-      env.stream_close
+        keepalive.cancel
+        env.stream_send("End of stream.")
+        env.stream_close
 
-      # keep checking if all parts are uploaded -> complete multipart -> save metadata
-    end
+        env.logger.info env['uploadinfo'].id
 
-    [200, {}, Goliath::Response::STREAMING]
+        # keep checking if all parts are uploaded -> complete multipart -> save metadata
+      end
+
+      [200, {}, Goliath::Response::STREAMING]
+    else
+      [422, {}, {error: "invalid upload"}]
+    end    
   end
 
   def on_close(env)
--- upload_info.rb
+++
@@ -1,6 +0,0 @@
-require 'glue'
-
-class UploadInfo
-  def initialize
-  end
-end
